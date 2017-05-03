143e51699a93bfeb81520a6d743e07bb33d4e6ec
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2017-05-03 02:02:25 +0200

    Cleared assets and contact tab

--- MyDescription/ContactViewController.swift
+++ MyDescription/ContactViewController.swift
@@ -1,20 +1,31 @@
 //
-//  MusicViewController.swift
+//  ContactViewController.swift
 //  MyDescription
 //
-//  Created by Levchuk Misha on 01.05.17.
+//  Created by Levchuk Misha on 03.05.17.
 //  Copyright © 2017 Levchuk Misha. All rights reserved.
 //
 
 import UIKit
 
-class MusicViewController: UIViewController {
+class ContactViewController: UIViewController {
 
-    
-    
+    @IBAction func fbLink(_ sender: AnyObject) {
+        if let url = NSURL(string: "https://www.facebook.com/misha.levchuk.1") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    @IBAction func vkLink(_ sender: Any) {
+        if let url = NSURL(string: "https://vk.com/id18167188") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    @IBAction func twLink(_ sender: Any) {
+        if let url = NSURL(string: "https://twitter.com/iX0ness") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
-    @IBOutlet weak var queenWebView: UIWebView!
-    @IBOutlet weak var acdcWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,10 +33,7 @@ class MusicViewController: UIViewController {
             menuBarButton.target = self.revealViewController()
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-            getVideo1(videoCode: "TXXO9_3gb3o")
-            getVideo2(videoCode: "jixYwUuW5U0")
         }
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,16 +41,6 @@ class MusicViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func getVideo1(videoCode:String)
-    {
-        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
-        acdcWebView.loadRequest(URLRequest(url: url!))
-    }
-    func getVideo2(videoCode:String)
-    {
-        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
-        queenWebView.loadRequest(URLRequest(url: url!))
-    }
 
    
 
