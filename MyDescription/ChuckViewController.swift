0597a3d766970938aa7c3b41b4b6d0b0577bfd58
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2017-05-04 16:30:45 +0200

    chuck json

---
+++ MyDescription/ChuckViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ChuckViewController.swift
+//  MyDescription
+//
+//  Created by Levchuk Misha on 04.05.17.
+//  Copyright © 2017 Levchuk Misha. All rights reserved.
+//
+
+import UIKit
+
+class ChuckViewController: UIViewController {
+
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        let url = URL(string: "https://api.chucknorris.io/jokes/random")
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("Poshel nachuj")
+            } else {
+                if let content = data {
+                    do {
+                        let myJSNON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJSNON)
+                        
+                        let joke = myJSNON["value"] as? String
+                        
+                        self.quoteLabel.text! = joke!
+                        
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
